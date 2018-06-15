515babe998977a614afef075f3976487051a2d4d
xieguigang <xie.guigang@gcmodeller.org>
2018-06-16 03:13:50 +0800

    add code comments

--- LINQ.ts/Linq/Enumerator.ts
+++ LINQ.ts/Linq/Enumerator.ts
@@ -6,6 +6,10 @@
     ToArray(): T[];
 }
 
+/**
+ * Provides a set of static (Shared in Visual Basic) methods for querying 
+ * objects that implement ``System.Collections.Generic.IEnumerable<T>``.
+*/
 class Enumerator<T> implements IEnumerable<T> {
 
     /**
@@ -17,14 +21,41 @@ class Enumerator<T> implements IEnumerable<T> {
         this.sequence = source;
     }
 
+    /**
+     * Projects each element of a sequence into a new form.
+     * 
+     * @typedef TOut The type of the value returned by selector.
+     * 
+     * @param selector A transform function to apply to each element.
+     * 
+     * @returns An ``System.Collections.Generic.IEnumerable<T>`` 
+     *          whose elements are the result of invoking the 
+     *          transform function on each element of source.
+    */
     public Select<TOut>(selector: (o: T) => TOut): Enumerator<TOut> {
         return Enumerable.Select<T, TOut>(this.sequence, selector);
     }
 
+    /**
+     * Sorts the elements of a sequence in ascending order according to a key.
+     * 
+     * @param key A function to extract a key from an element.
+     *
+     * @returns An ``System.Linq.IOrderedEnumerable<T>`` whose elements are 
+     *          sorted according to a key.
+    */
     public OrderBy(key: (e: T) => number): Enumerator<T> {
         return Enumerable.OrderBy(this.sequence, key);
     }
 
+    /**
+     * Sorts the elements of a sequence in descending order according to a key.
+     * 
+     * @param key A function to extract a key from an element.
+     * 
+     * @returns An ``System.Linq.IOrderedEnumerable<T>`` whose elements are 
+     *          sorted in descending order according to a key.
+    */
     public OrderByOrderByDescending(key: (e: T) => number): Enumerator<T> {
         return Enumerable.OrderByDescending(this.sequence, key);
     }
