e1565d4d6c468017cd6605794af5e9651e95f6d5
xieguigang <xie.guigang@gcmodeller.org>
2018-06-16 02:20:03 +0800

    add interface

---
+++ LINQ.ts/Linq/Enumerable.ts
@@ -0,0 +1,6 @@
+module Enumerable {
+
+    export function Select<T, TOut>(source: T[], project: (e: T) => TOut): Enumerator<TOut> {
+
+    }
+}
---
+++ LINQ.ts/Linq/Enumerator.ts
@@ -0,0 +1,16 @@
+﻿interface IEnumerable<T> {
+
+}
+
+class Enumerator<T> implements IEnumerable<T> {
+
+    private sequence: T[];
+
+    constructor(source: T[]) {
+        this.sequence = source;
+    }
+
+    public Select<TOut>(selector: (o: T) => TOut): Enumerator<TOut> {
+        return Enumerable.Select<T, TOut>(this.sequence, selector);
+    }
+}
