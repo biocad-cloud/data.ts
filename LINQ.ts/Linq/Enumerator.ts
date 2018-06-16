980d559a57037b33fd182c2548b7a24f36ad7f58
xieguigang <xie.guigang@gcmodeller.org>
2018-06-16 20:09:49 +0800

    split source files

--- LINQ.ts/Linq/Enumerator.ts
+++ LINQ.ts/Linq/Enumerator.ts
@@ -1,20 +1,4 @@
-﻿interface IEnumerable<T> {
-
-    readonly Count: number;
-    readonly [index: number]: T;
-
-    /**
-     * This function should returns a clone copy of the source sequence.
-    */
-    ToArray(): T[];
-}
-
-class Group<TKey, T> extends Enumerator<T> {
-    public Key: TKey;
-    public Group: T[];
-}
-
-/**
+﻿/**
  * Provides a set of static (Shared in Visual Basic) methods for querying 
  * objects that implement ``System.Collections.Generic.IEnumerable<T>``.
 */
---
+++ LINQ.ts/Linq/abstract.ts
@@ -0,0 +1,15 @@
+﻿interface IEnumerable<T> {
+
+    readonly Count: number;
+    readonly [index: number]: T;
+
+    /**
+     * This function should returns a clone copy of the source sequence.
+    */
+    ToArray(): T[];
+}
+
+class Group<TKey, T> extends Enumerator<T> {
+    public Key: TKey;
+    public Group: T[];
+}
