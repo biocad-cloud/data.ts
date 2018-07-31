3b9cbb6c1deceb9f0de87d3e3100f857c9f9d833
xieguigang <xie.guigang@gcmodeller.org>
2018-07-31 11:23:05 +0800

    range data model

---
+++ LINQ.ts/Range.ts
@@ -0,0 +1,14 @@
+﻿class DoubleRange {
+
+    public Min: number;
+    public Max: number;
+
+    public constructor(min: number, max: number) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public toString(): string {
+        return `[${this.min}, ${this.max}]`;
+    }
+}
