4653eb65a1f07faa0d9c627036734ad723d26a2e
xieguigang <xie.guigang@gcmodeller.org>
2018-07-31 11:45:55 +0800

    fix type name conflicts

--- LINQ.ts/Range.ts
+++ LINQ.ts/Range.ts
@@ -1,11 +1,19 @@
-﻿class DoubleRange {
+﻿class NumericRange implements DoubleRange {
 
-    public Min: number;
-    public Max: number;
+    public min: number;
+    public max: number;
 
     public constructor(min: number, max: number) {
         this.min = min;
         this.max = max;
+    }
+
+    public IsInside(x: number): boolean {
+        return x >= this.min && x <= this.max;
+    }
+
+    public static Create(min: number, max: number): NumericRange {
+        return new NumericRange(min, max);
     }
 
     public toString(): string {
