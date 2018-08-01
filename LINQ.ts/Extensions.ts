24f42b45dd64b5a155c2f3fe1c8309fe4645ba6f
xieguigang <xie.guigang@gcmodeller.org>
2018-08-01 08:40:52 +0800

    is null or empty

--- LINQ.ts/Extensions.ts
+++ LINQ.ts/Extensions.ts
@@ -3,3 +3,13 @@
 function From<T>(source: T[]): IEnumerator<T> {
     return new IEnumerator<T>(source);
 }
+
+function IsNullOrEmpty<T>(array: T[]): boolean {
+    if (array == null || array == undefined) {
+        return true;
+    } else if (array.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
