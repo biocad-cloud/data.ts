2c5b7a12a7ce7012a811c7d0658acfb63664045d
xieguigang <xie.guigang@gcmodeller.org>
2018-08-01 09:12:45 +0800

    add code comments

--- LINQ.ts/BinaryTree/binaryTree.ts
+++ LINQ.ts/BinaryTree/binaryTree.ts
@@ -1,7 +1,21 @@
-﻿
+﻿/**
+ * 用于进行数据分组所需要的最基础的二叉树数据结构
+*/
 class binaryTree<T, V> {
 
+    /**
+     * 根节点，根节点的key值可能会对二叉树的构建造成很大的影响
+    */
     public root: node<T, V>;
+    /**
+     * 这个函数指针描述了如何对两个``key``之间进行比较
+     * 
+     * 返回结果值：
+     * 
+     * + ``等于0`` 表示二者相等
+     * + ``大于0`` 表示a大于b
+     * + ``小于0`` 表示a小于b
+    */
     public compares: (a: T, b: T) => number;
 
     constructor(comparer: (a: T, b: T) => number) {
@@ -46,6 +60,9 @@ class binaryTree<T, V> {
         }
     }
 
+    /**
+     * 根据key值查找一个节点，然后获取该节点之中与key所对应的值
+    */
     public find(term: T): V {
         var np = this.root;
         var cmp = 0;
