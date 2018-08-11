289d1a38efaa7d5b5b4adbddb3d554ea4eca5ae4
xieguigang <xie.guigang@gcmodeller.org>
2018-08-11 15:06:34 +0800

    add code comments

--- LINQ.ts/Linq/DictionaryMaps.ts
+++ LINQ.ts/Linq/DictionaryMaps.ts
@@ -3,28 +3,51 @@
 */
 class Map<K, V> {
 
-    public Key: K;
+    /**
+     * 键名称，一般是字符串
+    */
+    public key: K;
+    /**
+     * 目标键名所映射的值
+    */
     public value: V;
 
+    /**
+     * 创建一个新的键值对集合
+     * 
+    */
     public constructor(key: K = null, value: V = null) {
-        this.Key = key;
+        this.key = key;
         this.value = value;
     }
 
     public toString(): string {
-        return `[${this.Key.toString()}, ${this.value.toString()}]`;
+        return `[${this.key.toString()}, ${this.value.toString()}]`;
     }
 }
 
+/**
+ * 键值对映射哈希表
+*/
 class Dictionary<V> extends IEnumerator<Map<string, V>>  {
 
     private maps: object;
 
+    public Item(key: string): V {
+        return <V>(this.maps[key]);
+    }
+
+    /**
+     * 将目标对象转换为一个类型约束的映射序列集合
+    */
     public constructor(maps: object) {
         super(Dictionary.ObjectMaps<V>(maps));
         this.maps = maps;
     }
 
+    /**
+     * 将目标对象转换为一个类型约束的映射序列集合
+    */
     public static ObjectMaps<V>(maps: object): Map<string, V>[] {
         return From(Object.keys(maps))
             .Select(key => new Map<string, V>(key, maps[key]))
--- LINQ.ts/Linq/Group.ts
+++ LINQ.ts/Linq/Group.ts
@@ -13,4 +13,13 @@
         super(group);
         this.Key = key;
     }
+
+    /**
+     * 创建一个键值对映射序列，这些映射都具有相同的键名
+    */
+    public ToMaps(): Map<TKey, T>[] {
+        return From(this.sequence)
+            .Select(x => new Map<TKey, T>(this.Key, x))
+            .ToArray();
+    }
 }
