17642265095612cbac7be74bb41bff8613d290ed
xieguigang <xie.guigang@gcmodeller.org>
2018-08-18 19:42:33 +0800

    add code comments

--- Linq/Pointer.ts
+++ Linq/Pointer.ts
@@ -5,16 +5,29 @@
 */
 class Pointer<T> extends IEnumerator<T> {
 
+    /**
+     * The index pointer of the current data sequence.
+    */
     public i: number;
 
+    /**
+     * The index pointer is at the end of the data sequence?
+    */
     public get EndRead(): boolean {
         return this.i >= this.Count;
     }
 
+    /**
+     * Get the element value in current location i;
+    */
     public get Current(): T {
         return this.sequence[this.i];
     }
 
+    /**
+     * Get current index element value and then move the pointer 
+     * to next position.
+    */
     public get Next(): T {
         var x = this.Current;
         this.i = this.i + 1;
@@ -25,6 +38,10 @@ class Pointer<T> extends IEnumerator<T> {
         super(src);
     }
 
+    /**
+     * Just move the pointer to the next position and then 
+     * returns current pointer object.
+    */
     public MoveNext(): Pointer<T> {
         this.i = this.i + 1;
         return this;
