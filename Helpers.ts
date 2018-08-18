f2e81b332e4a73dbdfacac8635830e24898cac4e
xieguigang <xie.guigang@gcmodeller.org>
2018-08-18 17:37:21 +0800

    csv data IO

---
+++ Helpers.ts
@@ -0,0 +1,35 @@
+﻿module LinqHelpers {
+
+    export function POST(url: string, postData: PostData, callback: (response: string) => void) {
+        var http = new XMLHttpRequest();
+        var data: any = postData.data;
+
+        http.open('POST', url, true);
+        // Send the proper header information along with the request
+        http.setRequestHeader('Content-type', postData.type);
+        // Call a function when the state changes.
+        http.onreadystatechange = function () {
+            if (http.readyState == 4 && http.status == 200) {
+                callback(http.responseText);
+            }
+        }
+        http.send(data);
+    }
+
+    export function UploadFile(url: string, postData: PostData, callback: (response: string) => void) {
+
+    }
+
+    export class PostData {
+
+        /**
+         * content type
+        */
+        public type: string;
+        public data: any;
+
+        public toString(): string {
+            return this.type;
+        }
+    }
+}
--- csv/Dataframe.ts
+++ csv/Dataframe.ts
@@ -1,4 +1,12 @@
-﻿namespace csv {
+﻿/**
+ * http://www.rfc-editor.org/rfc/rfc4180.txt
+*/
+namespace csv {
+
+    /**
+     * Common Format and MIME Type for Comma-Separated Values (CSV) Files
+    */
+    const contentType: string = "text/csv";
 
     /**
      * ``csv``文件模型
@@ -14,6 +22,48 @@
 
         public constructor(rows: row[]) {
             super(rows);
+        }
+
+        public buildDoc(): string {
+            return this.Select(r => r.rowLine).JoinBy("\n");
+        }
+
+        /**
+         * 使用ajax将csv文件保存到服务器
+         * 
+         * @param url csv文件数据将会被通过post方法保存到这个url所指定的网络资源上面
+         * @param callback ajax异步回调，默认是打印返回结果到终端之上
+         * 
+        */
+        public save(
+            url: string,
+            callback: (response: string) => void =
+                (response: string) => {
+                    console.log(response);
+                }): void {
+
+            var file: string = this.buildDoc();
+            var data = <LinqHelpers.PostData>{
+                type: contentType,
+                data: file
+            };
+
+            LinqHelpers.UploadFile(url, data, callback);
+        }
+
+        /**
+         * 使用ajax GET加载csv文件数据，不推荐使用这个方法处理大型的csv文件数据
+         * 
+         * @param callback 当这个异步回调为空值的时候，函数使用同步的方式工作，返回csv对象
+         *                 如果这个参数不是空值，则以异步的方式工作，此时函数会返回空值
+        */
+        public static Load(url: string, callback: (csv: dataframe) => void = null): dataframe {
+            if (callback == null || callback == undefined) {
+                // 同步
+            } else {
+                // 异步
+                return null;
+            }
         }
     }
 }
