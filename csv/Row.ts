c0a9225f4a3e5af82e4db292fea0932615d949ca
xieguigang <xie.guigang@gcmodeller.org>
2018-08-18 09:08:08 +0800

    escape cell data

--- csv/Row.ts
+++ csv/Row.ts
@@ -8,11 +8,17 @@
          * 这个只读属性仅用于生成csv文件
         */
         public get rowLine(): string {
-            return From(this.columns).Select(row.autoEscape).JoinBy(",");
+            return From(this.columns)
+                .Select(row.autoEscape)
+                .JoinBy(",");
         }
 
         private static autoEscape(c: string): string {
-
+            if (c.indexOf(",") > -1) {
+                return `"${c}"`;
+            } else {
+                return c;
+            }
         }
     }
 }
