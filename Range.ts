0aa301f01d89369246745658e1e78c618216d427
xieguigang <xie.guigang@gcmodeller.org>
2018-08-26 01:36:51 +0800

    file reorganized

--- Range.ts
+++ Range.ts
@@ -1,35 +1,40 @@
-﻿
-/**
+﻿/**
  * 一个数值范围
 */
 class NumericRange implements DoubleRange {
+	// #region Properties (2)
 
-    /**
-     * 这个数值范围的最小值
-    */
-    public min: number;
-    /**
+	/**
      * 这个数值范围的最大值
     */
     public max: number;
+	/**
+     * 这个数值范围的最小值
+    */
+    public min: number;
 
-    public get Length(): number {
-        return this.max - this.min;
-    }
+	// #endregion
 
-    public constructor(min: number, max: number) {
+	// #region Constructors (1)
+
+	public constructor(min: number, max: number) {
         this.min = min;
         this.max = max;
     }
 
-    /**
-     * 判断目标数值是否在当前的这个数值范围之内
-    */
-    public IsInside(x: number): boolean {
-        return x >= this.min && x <= this.max;
+	// #endregion
+
+	// #region Public Accessors (1)
+
+	public get Length(): number {
+        return this.max - this.min;
     }
 
-    /**
+	// #endregion
+
+	// #region Public Static Methods (1)
+
+	/**
      * 从一个数值序列之中创建改数值序列的值范围
     */
     public static Create(numbers: number[]): NumericRange {
@@ -40,7 +45,18 @@ class NumericRange implements DoubleRange {
         return new NumericRange(min, max);
     }
 
-    public PopulateNumbers(step: number = (this.Length / 10)): number[] {
+	// #endregion
+
+	// #region Public Methods (3)
+
+	/**
+     * 判断目标数值是否在当前的这个数值范围之内
+    */
+    public IsInside(x: number): boolean {
+        return x >= this.min && x <= this.max;
+    }
+
+	public PopulateNumbers(step: number = (this.Length / 10)): number[] {
         var data: number[] = [];
 
         for (var x: number = this.min; x < this.max; x += step) {
@@ -50,7 +66,9 @@ class NumericRange implements DoubleRange {
         return data;
     }
 
-    public toString(): string {
+	public toString(): string {
         return `[${this.min}, ${this.max}]`;
     }
+
+	// #endregion
 }
