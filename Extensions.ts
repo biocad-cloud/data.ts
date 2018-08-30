3dc4fdec4436cc81f046e60357ea9a16797b8df8
xieguigang <xie.guigang@gcmodeller.org>
2018-08-30 15:43:51 +0800

    split source file

--- Extensions.ts
+++ Extensions.ts
@@ -27,25 +27,6 @@ function IsNullOrEmpty<T>(array: T[] | IEnumerator<T>): boolean {
 }
 
 /**
- * 判断给定的字符串是否是空值？
- * 
- * @param stringAsFactor 假若这个参数为真的话，那么字符串``undefined``也将会被当作为空值处理
-*/
-function StringEmpty(str: string, stringAsFactor = false): boolean {
-    if (!str) {
-        return true;
-    } else if (str == undefined) {
-        return true;
-    } else if (str.length == 0) {
-        return true;
-    } else if (stringAsFactor && str.toString() == "undefined") {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-/**
  * HTML/Javascript: how to access JSON data loaded in a script tag.
 */
 function LoadJson(id: string): any {
---
+++ Helpers/Strings.ts
@@ -0,0 +1,21 @@
+﻿module Strings {
+
+    /**
+     * 判断给定的字符串是否是空值？
+     * 
+     * @param stringAsFactor 假若这个参数为真的话，那么字符串``undefined``也将会被当作为空值处理
+    */
+    export function Empty(str: string, stringAsFactor = false): boolean {
+        if (!str) {
+            return true;
+        } else if (str == undefined) {
+            return true;
+        } else if (str.length == 0) {
+            return true;
+        } else if (stringAsFactor && str.toString() == "undefined") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
