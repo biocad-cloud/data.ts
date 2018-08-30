cf37e0df84e855a8018d93a8afbc0090e9213742
xieguigang <xie.guigang@gcmodeller.org>
2018-08-30 17:20:55 +0800

    get type members

--- Linq.ts/Linq/Type.ts
+++ Linq.ts/Linq/Type.ts
@@ -1,11 +1,17 @@
-﻿class TypeInfo {
+﻿/**
+ * 类似于反射类型
+*/
+class TypeInfo {
 
-    public TypeOf: string;
+    public readonly TypeOf: string;
 
     /**
      * 如果这个属性是空的，则说明是js之中的基础类型
     */
-    public Class: string;
+    public readonly Class: string;
+
+    public readonly property: string[];
+    public readonly methods: string[];
 
     /**
      * 是否是js之中的基础类型？
@@ -18,10 +24,27 @@
      * 获取某一个对象的类型信息
     */
     public static typeof<T>(obj: T): TypeInfo {
+        var type = typeof obj;
+        var isObject: boolean = type == "object";
+
         return <TypeInfo>{
             TypeOf: typeof obj,
-            Class: (typeof obj == "object") ? obj.constructor.toString() : ""
+            Class: isObject ? (<any>obj.constructor).name : "",
+            property: Object.keys(obj),
+            methods: TypeInfo.GetObjectMethods(obj)
         };
+    }
+
+    public static GetObjectMethods<T>(obj: T): string[] {
+        var res: string[] = [];
+
+        for (var m in obj) {
+            if (typeof obj[m] == "function") {
+                res.push(m)
+            }
+        }
+
+        return res;
     }
 
     public toString() {
