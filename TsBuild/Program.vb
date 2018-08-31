cd2a4ce775d3d95e2234f8a99357705b81ff9f6b
xieguigang <xie.guigang@gcmodeller.org>
2018-09-01 00:07:32 +0800

    overrides tsconfig file

--- TsBuild/Program.vb
+++ TsBuild/Program.vb
@@ -1,8 +1,9 @@
 ﻿Imports System.ComponentModel
+Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
-Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio
 Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Serialization.JSON
 
 Module Program
 
@@ -15,7 +16,7 @@ Module Program
     <Usage("/compile /proj <*.njsproj> [/out <output.js>]")>
     Public Function Compile(args As CommandLine) As Integer
         Dim in$ = args <= "/proj"
-        Dim out$ = args("/out") Or $"{[in].ParentPath}/tsconfig.json"
+        Dim out$ = args("/out") Or ""
 
         ' njsproj -> code files -> tsconfig.json => files -> tsc
         Dim njsproj As Project = [in].LoadXml(Of Project)
@@ -29,7 +30,11 @@ Module Program
                                      End Function) _
                               .Select(Function(tsc) tsc.Include) _
                               .ToArray
+        ' 接下来将会覆盖掉tsconfig之中的files数组
+        Dim config$ = $"{[in].ParentPath}/tsconfig.json"
+        Dim tsconfig As tsconfig = config.LoadJSON(Of tsconfig)
 
-
+        tsconfig.files = codes
+        tsconfig.savejson(config)
     End Function
 End Module
