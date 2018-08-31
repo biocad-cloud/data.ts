d06f2624b75a199757bf005831a6def784023001
xieguigang <xie.guigang@gcmodeller.org>
2018-08-31 23:52:44 +0800

    load njsproj

--- TsBuild/Program.vb
+++ TsBuild/Program.vb
@@ -1,7 +1,35 @@
-﻿Module Program
+﻿Imports System.ComponentModel
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.ApplicationServices.Development.VisualStudio
+Imports Microsoft.VisualBasic.Linq
 
-    Sub Main()
+Module Program
 
-    End Sub
+    Public Function Main() As Integer
+        Return GetType(Program).RunCLI(App.CommandLine)
+    End Function
 
+    <ExportAPI("/compile")>
+    <Description("https://www.typescriptlang.org/docs/handbook/tsconfig-json.html")>
+    <Usage("/compile /proj <*.njsproj> [/out <output.js>]")>
+    Public Function Compile(args As CommandLine) As Integer
+        Dim in$ = args <= "/proj"
+        Dim out$ = args("/out") Or $"{[in].ParentPath}/tsconfig.json"
+
+        ' njsproj -> code files -> tsconfig.json => files -> tsc
+        Dim njsproj As Project = [in].LoadXml(Of Project)
+        Dim codes$() = njsproj.ItemGroups _
+                              .Select(Function(item)
+                                          Return item.TypeScriptCompiles.SafeQuery
+                                      End Function) _
+                              .IteratesALL _
+                              .Where(Function(tsc)
+                                         Return tsc.SubType = "Code"
+                                     End Function) _
+                              .Select(Function(tsc) tsc.Include) _
+                              .ToArray
+
+
+    End Function
 End Module
---
+++ TsBuild/tsconfig.vb
@@ -0,0 +1,3 @@
+﻿Public Class tsconfig
+
+End Class
