b4b72ee0e178a0a33261a6c54ff6e0c2394444c2
xieguigang <xie.guigang@gcmodeller.org>
2018-08-31 16:38:11 +0800

    create slide windows

--- Linq.ts/Data/SlideWindow.ts
+++ Linq.ts/Data/SlideWindow.ts
@@ -11,5 +11,32 @@
             super(src);
             this.index = index;
         }
+
+        /**
+         * 创建指定片段长度的滑窗对象
+         * 
+         * @param winSize 滑窗片段的长度
+         * @param step 滑窗的步进长度，默认是一个步进
+        */
+        public static Split<T>(src: T[] | IEnumerator<T>, winSize: number, step: number = 1): IEnumerator<SlideWindow<T>> {
+            if (!Array.isArray(src)) {
+                src = src.ToArray();
+            }
+
+            var len: number = src.length - winSize;
+            var windows: SlideWindow<T>[];
+
+            for (var i: number = 0; i < len; i += step) {
+                var chunk: T[] = new Array(winSize);
+
+                for (var j: number = 0; j < winSize; j++) {
+                    chunk[j] = src[i + j];
+                }
+
+                windows.push(new SlideWindow<T>(i, chunk));
+            }
+
+            return new IEnumerator<SlideWindow<T>>(windows);
+        }
     }
 }
