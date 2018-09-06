8bddb46a972f96481cca22051a50645077643889
xieguigang <xie.guigang@gcmodeller.org>
2018-09-06 14:23:11 +0800

    add event helper

--- Linq.ts/Linq/DOM/Document.ts
+++ Linq.ts/Linq/DOM/Document.ts
@@ -21,4 +21,29 @@
 
         return new IEnumerator<HTMLElement>(list);
     }
+
+    /**
+     *
+    */
+    export function addEvent(el: any, type: string, fn: (event) => void): void {
+        if (document.addEventListener) {
+            if (el && (el.nodeName) || el === window) {
+                el.addEventListener(type, fn, false);
+            } else if (el && el.length) {
+                for (var i = 0; i < el.length; i++) {
+                    addEvent(el[i], type, fn);
+                }
+            }
+        } else {
+            if (el && el.nodeName || el === window) {
+                el.attachEvent('on' + type, () => {
+                    return fn.call(el, window.event);
+                });
+            } else if (el && el.length) {
+                for (var i = 0; i < el.length; i++) {
+                    addEvent(el[i], type, fn);
+                }
+            }
+        }
+    }
 }
