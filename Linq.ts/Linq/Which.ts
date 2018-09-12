88eeab2f84b3bbae371b397973297ff8aa614eb3
xieguigang <xie.guigang@gcmodeller.org>
2018-09-12 08:42:39 +0800

    which max

---
+++ Linq.ts/Linq/Which.ts
@@ -0,0 +1,22 @@
+﻿module Which {
+
+    export function Max<T>(
+        x: IEnumerator<T>,
+        compare: (a: T, b: T) => number = (a, b) => {
+            return DataExtensions.as_numeric(a) - DataExtensions.as_numeric(b);
+        }): number {
+
+        var xMax: T = null;
+        var iMax: number = 0;
+
+        for (var i: number = 0; i < x.Count; i++) {
+            if (compare(x.ElementAt(i), xMax) > 0) {
+                // x > xMax
+                xMax = x.ElementAt(i);
+                iMax = i;
+            }
+        }
+
+        return iMax;
+    }
+}
