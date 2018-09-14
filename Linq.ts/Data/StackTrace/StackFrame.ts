13492e859a4a98f4805f0def53d9ef5e42d17267
xieguigang <xie.guigang@gcmodeller.org>
2018-09-14 13:19:43 +0800

    fix caller stack parser bugs

--- Linq.ts/Data/StackTrace/StackFrame.ts
+++ Linq.ts/Data/StackTrace/StackFrame.ts
@@ -16,17 +16,25 @@
             var frame: StackFrame = new StackFrame();
             var file: string = StackFrame.getFileName(line);
             var caller = line.replace(file, "").trim().substr(3);
-            console.log(file);
+           
             file = file.substr(1, file.length - 2);
 
-            var position: string = file.match(/([:]\d+){2}$/m)[0];
-            var location = From(position.split(":"))
+            if (caller.indexOf("/") > -1 || caller.indexOf(":") > -1) {
+                // 没有替换成功，任然是一个文件路径，则可能
+                // 是html文档之中的一个最开始的函数调用
+                // 是没有caller的
+                caller = "<HTML\\Document>";
+            }
+
+            var position: IEnumerator<string> = $ts(file.match(/([:]\d+){2}$/m)[0].split(":"));
+            var posStrLen: number = (position.Select(s => s.length).Sum() + 2);
+            var location = From(position)
                 .Where(s => s.length > 0)
-                .Select(parseInt)
+                .Select(x => Strings.Val(x))
                 .ToArray();
 
-            frame.file = file.substr(0, file.length - position.length);
-
+            frame.file = file.substr(0, file.length - posStrLen);
+            
             var alias: RegExpMatchArray = caller.match(/\[.+\]/);
             var memberName = (!alias || alias.length == 0) ? null : alias[0];
 
