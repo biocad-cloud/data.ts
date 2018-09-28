183eb58fe088eafa47eb276514b5e29d82d39496
xieguigang <xie.guigang@gcmodeller.org>
2018-09-28 10:09:16 +0800

    goto link

--- Linq.ts/Linq/Router.ts
+++ Linq.ts/Linq/Router.ts
@@ -6,24 +6,30 @@
         return frame;
     }
 
+    const routerLink: string = "router-link";
+
     export function register(appId: string = "app") {
-        var a = <Linq.DOM.DOMEnumerator<HTMLAnchorElement>>$ts(".router");
+        var aLinks: Linq.DOM.DOMEnumerator<HTMLAnchorElement>;
+
         frame = $ts(`<iframe id="${appId}-frame">`);
+        aLinks = $ts(".router");
 
         (<HTMLElement>$ts(`#${appId}`)).appendChild(frame);
 
-        a.ForEach(link => link.setAttribute("router-link", link.href));
-        a.AddEvent("onclick", (link, click) => {
-
+        aLinks.attr("router-link", link => link.href);
+        aLinks.attr("href", "javascript:void(0)");
+        aLinks.AddEvent("onclick", (link, click) => {
+            Router.goto(link.getAttribute("router-link"));
         });
     }
 
-    export function goto(link: string, stack: Window) {
+    export function goto(link: string, stack: Window = null) {
         if (parent) {
             Router.goto(link, parent);
         } else {
             // 没有parent了，已经到达最顶端了
-            var frame: HTMLIFrameElement = (<any>stack).Router.iFrame();
+            var frame: HTMLIFrameElement;
+            frame = (<any>stack).Router.iFrame();
             frame.src = `${link}&refresh=${Math.random()}`;
         }
     }
