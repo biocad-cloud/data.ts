ee50998495af146dcf73ecbca07d7c7e125954b5
xieguigang <xie.guigang@gcmodeller.org>
2018-10-10 10:01:23 +0800

    is numeric pattern

--- Linq.ts/Helpers/Strings.ts
+++ Linq.ts/Helpers/Strings.ts
@@ -5,6 +5,12 @@ module Strings {
 
     export const x0: number = "0".charCodeAt(0);
     export const x9: number = "9".charCodeAt(0);
+    export const numericPattern: RegExp = /[-]?\d+(\.\d+)?/g;
+
+    export function isNumericPattern(text: string): boolean {
+        var match = Strings.numericPattern.exec(text)[0];
+        return match == text;
+    }
 
     /**
      * @param text A single character
