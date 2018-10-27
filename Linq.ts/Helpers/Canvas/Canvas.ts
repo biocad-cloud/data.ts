9a0d5d7869383076103dcaea88c3f2dfbcfe8ddf
xieguigang <xie.guigang@gcmodeller.org>
2018-10-27 14:31:15 +0800

    image OK helper

--- Linq.ts/Helpers/Canvas/Canvas.ts
+++ Linq.ts/Helpers/Canvas/Canvas.ts
@@ -22,7 +22,32 @@ namespace CanvasHelper {
 
         return metrics.width;
     }
-       
+
+    /**
+     * found this trick at http://talideon.com/weblog/2005/02/detecting-broken-images-js.cfm
+    */
+    export function imageOk(img: HTMLImageElement): boolean {
+        "use strict";
+
+        // During the onload event, IE correctly identifies any images that
+        // weren't downloaded as not complete. Others should too. Gecko-based
+        // browsers act like NS4 in that they report this incorrectly.
+        if (!img.complete) {
+            return false;
+        }
+
+        // However, they do have two very useful properties: naturalWidth and
+        // naturalHeight. These give the true size of the image. If it failed
+        // to load, either of these should be zero.
+        if (typeof img.naturalWidth !== "undefined" && img.naturalWidth === 0) {
+            return false;
+        }
+
+        // No other way of checking: assume it's ok.
+        return true;
+    }
+
+
     export class fontSize {
 
         public point: number;
@@ -31,7 +56,7 @@ namespace CanvasHelper {
         public percent: number;
 
         public readonly sizes: fontSize[] = [
-            
+
         ];
     }
 }
