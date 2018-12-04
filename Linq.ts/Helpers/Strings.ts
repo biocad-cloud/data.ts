ed3a7b56874a829b9e4d945cb2e4cecc57327eed
xieguigang <xie.guigang@gcmodeller.org>
2018-12-04 14:22:20 +0800

    number round to string

--- Linq.ts/Helpers/Strings.ts
+++ Linq.ts/Helpers/Strings.ts
@@ -15,6 +15,21 @@ module Strings {
     }
 
     /**
+     * 默认是保留3位有效数字的
+    */
+    export function round(x: number | string, decimals: number = 3) {
+        var f_x = typeof x == "number" ? x : parseFloat(x);
+        var n = Math.pow(10, decimals);
+
+        if (isNaN(f_x)) {
+            console.warn(`Invalid number value: '${x}'`);
+            return false;
+        } else {
+            return Math.round(x * n) / n;
+        }
+    }
+
+    /**
      * @param text A single character
     */
     export function isNumber(text: string): boolean {
