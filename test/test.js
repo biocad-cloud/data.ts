19ee1acd557da607fda06769c9d31ca9781dace6
xieguigang <xie.guigang@gcmodeller.org>
2018-12-17 18:52:44 +0800

    add dom query test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 /// <reference path="../../../build/linq.d.ts" />
 console.log('Hello world');
 console.log($ts(["1", "2", "3"]).ElementType);
+console.log($ts("#T5555").display("23333<sup>33333</sup>"));
+console.log($ts(".gg").onClick(function (sender) { return alert(sender.innerText); }));
 console.log("");
 //# sourceMappingURL=test.js.map
--- test/test/app.ts
+++ test/test/app.ts
@@ -4,4 +4,7 @@ console.log('Hello world');
 
 console.log($ts(["1", "2", "3"]).ElementType);
 
+console.log($ts("#T5555").display("23333<sup>33333</sup>"));
+console.log((<DOMEnumerator<HTMLElement>><any>$ts(".gg")).onClick((sender) => alert(sender.innerText)));
+
 console.log("");
