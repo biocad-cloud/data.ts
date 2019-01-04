c6a7d0bbe46deaa5238ab8d7f0202a8e2579a016
xieguigang <xie.guigang@gcmodeller.org>
2019-01-04 09:19:46 +0800

    add iterator model

---
+++ Linq.ts/Collections/Abstract/Iterator.ts
@@ -0,0 +1,52 @@
+﻿/**
+  
+  var it = makeIterator(['a', 'b']);
+
+  it.next() // { value: "a", done: false }
+  it.next() // { value: "b", done: false }
+  it.next() // { value: undefined, done: true }
+
+  function makeIterator(array) {
+      var nextIndex = 0;
+  
+      return {
+          next: function() {
+              return nextIndex < array.length ?
+                  {value: array[nextIndex++], done: false} :
+                  {value: undefined, done: true};
+          }
+      };
+  }
+
+*/
+
+class Iterator<T> {
+
+    protected sequence: T[];
+
+    private index: number = 0;
+
+    public get Count(): number {
+        return this.sequence.length;
+    }
+
+    public constructor(array: T[]) {
+        this.sequence = array;
+    }
+
+    public reset(): Iterator<T> {
+        this.index = 0;
+        return this;
+    }
+
+    public next(): IPopulated<T> {
+        return this.index < this.sequence.length ?
+            <IPopulated<T>>{ value: this.sequence[this.index++], done: false } :
+            <IPopulated<T>>{ value: undefined, done: true };
+    }
+}
+
+interface IPopulated<T> {
+    value: T;
+    done: boolean;
+}
