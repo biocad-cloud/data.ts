8a8e8191c521e89d0381e1635bc725c2dcc9270d
xieguigang <xie.guigang@gcmodeller.org>
2019-03-09 18:40:52 +0800

    improvements of the data helper

---
+++ Linq.ts/Collections/Matrix.ts
@@ -0,0 +1,17 @@
+﻿class Matrix<T> {
+
+    private matrix: T[][];
+
+    /**
+     * [m, n], m列n行
+    */
+    public constructor(m: number, n: number) {
+        this.matrix = [];
+
+        for (var i: number = 0; i < n; i++) {
+            this.matrix.push();
+        }
+    }
+
+
+}
