edc7264d180e3f1902c6cdd8902ad556a3725eb5
xieguigang <xie.guigang@gcmodeller.org>
2019-03-14 14:53:08 +0800

    update runtime

---
+++ TsBuild/wasm.vb/Types.vb
@@ -0,0 +1,10 @@
+﻿Public Class Types
+
+    Public ReadOnly Property Convert2Wasm As New Dictionary(Of Type, String) From {
+        {GetType(Integer), "i32"},
+        {GetType(Long), "i64"},
+        {GetType(Single), "f32"},
+        {GetType(Double), "f64"}
+    }
+
+End Class
