37b644a35b8e9103799a08d923d1c4cfb2137623
xieguigang <xie.guigang@gcmodeller.org>
2019-03-14 15:24:04 +0800

    add sexpression models

---
+++ TsBuild/wasm.vb/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub test()
+
+        Dim src = "
+Module Test1
+
+Public Function add(x As Integer, y As Integer) As Double
+    Return x + y
+End Function
+
+End Module
+"
+
+
+    End Sub
+End Module
