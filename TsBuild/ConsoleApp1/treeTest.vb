dac0998c500c7fd43b2345366129b55344aa31f0
xieguigang <xie.guigang@gcmodeller.org>
2019-03-24 18:44:03 +0800

    add VB code parser test

---
+++ TsBuild/ConsoleApp1/treeTest.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.VisualBasic
+Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+
+Module treeTest
+
+    Sub Main()
+        Dim code = "Module MNain
+
+Public Sub Main
+Console.WriteLine(1+1)
+End Sub
+
+End Module"
+        Dim tree As SyntaxTree = VisualBasicSyntaxTree.ParseText(code)
+
+        Dim root As CompilationUnitSyntax = tree.GetRoot
+
+
+        Pause()
+    End Sub
+
+End Module
