e28cd69286061114175f38e3cfee094e39ad4ed9
xieguigang <xie.guigang@gcmodeller.org>
2019-03-24 20:01:18 +0800

    parse tree

--- TsBuild/Wasm/treeTest.vb
+++ TsBuild/Wasm/treeTest.vb
@@ -1,14 +1,15 @@
 ﻿Imports Microsoft.CodeAnalysis
 Imports Microsoft.CodeAnalysis.VisualBasic
 Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 
 Module treeTest
 
     Sub Main()
         Dim code = "Module MNain
 
-Public Sub Main
-Console.WriteLine(1+1)
+Public Function Main(x As Integer, Optional y& = 99) As Long
+Return (1+1 + x) * y ^2
 End Sub
 
 End Module"
@@ -16,8 +17,36 @@ End Module"
 
         Dim root As CompilationUnitSyntax = tree.GetRoot
 
+        Dim main As ModuleBlockSyntax = root.Members(Scan0)
+
+        For Each api_method In main.Members.OfType(Of MethodBlockSyntax)
+            Dim parameters = api_method.BlockStatement.ParameterList.Parameters.Select(AddressOf GetParameterType).ToArray
+            Dim name = api_method.SubOrFunctionStatement.Identifier.ValueText
+            Dim returns = GetAsType(api_method.SubOrFunctionStatement.AsClause)
+            Dim body = api_method.Statements.ToArray
+
+            Pause()
+        Next
 
         Pause()
     End Sub
+
+    Public Function GetAsType([as] As SimpleAsClauseSyntax) As String
+        Return DirectCast([as].Type, PredefinedTypeSyntax).Keyword.ValueText
+    End Function
+
+    Public Function GetParameterType(parameter As ParameterSyntax) As NamedValue(Of String)
+        Dim name = parameter.Identifier.Identifier.Text
+        Dim type As String
+
+        If parameter.AsClause Is Nothing Then
+            type = name.Last
+            name = name.Substring(0, name.Length - 1)
+        Else
+            type = GetAsType(parameter.AsClause)
+        End If
+
+        Return New NamedValue(Of String)(name, type)
+    End Function
 
 End Module
