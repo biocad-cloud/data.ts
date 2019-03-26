b0302a68f437d918f586cbf3374ad0b1959cad57
xieguigang <xie.guigang@gcmodeller.org>
2019-03-26 18:44:21 +0800

    imports function is also require data type

--- TsBuild/Wasm/Symbols/ImportSymbol.vb
+++ TsBuild/Wasm/Symbols/ImportSymbol.vb
@@ -1,14 +1,38 @@
-﻿Namespace Symbols
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Namespace Symbols
     Public Class ImportSymbol : Inherits FuncSignature
 
         Public Property Package As String
         Public Property ImportObject As String
+
+        Sub New()
+        End Sub
+
+        Sub New(ParamArray args As NamedValue(Of String)())
+            Parameters = args
+        End Sub
 
         Public Overrides Function ToSExpression() As String
             Dim params$ = Parameters.Select(Function(a) a.param).JoinBy(" ")
 
             Return $"(func ${Name} (import ""{Package}"" ""{ImportObject}"") {params} (result {Result}))"
         End Function
+
+        Public Shared ReadOnly Property MathImports As ImportSymbol()
+            <MethodImpl(MethodImplOptions.AggressiveInlining)>
+            Get
+                Return mathImport().ToArray
+            End Get
+        End Property
+
+        Private Shared Iterator Function mathImport() As IEnumerable(Of ImportSymbol)
+            Const Math$ = NameOf(Math)
+
+            Yield New ImportSymbol With {.Package = Math, .ImportObject = "", .Name = "", .Parameters = {}, .Result = ""}
+        End Function
+
     End Class
 End Namespace
 
