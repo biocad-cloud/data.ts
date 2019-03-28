b34d65afcd2a5dec5cf9e7dc908140ebdcbf928a
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-03-28 18:05:01 +0800

    add compiler solution

---
+++ TsBuild/vbw/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WebAssembly compiler for VisualBasic")>
+<Assembly: AssemblyDescription("WebAssembly compiler for VisualBasic")>
+<Assembly: AssemblyCompany("MIT Licensed")>
+<Assembly: AssemblyProduct("VB WebAssembly")>
+<Assembly: AssemblyCopyright("Copyright © I@xieguigang.me 2019")>
+<Assembly: AssemblyTrademark("sciBASIC.NET")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8525fa5f-ecfe-4b42-96b2-8363f301dcba")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
