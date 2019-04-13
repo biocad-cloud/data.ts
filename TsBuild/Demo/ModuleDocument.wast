9d94c7ec1009af60d4e3ea4040711385c8dd3d22
xieguigang <xie.guigang@gcmodeller.org>
2019-04-13 20:02:33 +0800

    fix for function parameter type

---
+++ TsBuild/Demo/ModuleDocument.wast
@@ -0,0 +1,59 @@
+(module ;; Module ModuleDocument
+
+    ;; Auto-Generated VisualBasic.NET WebAssembly Code
+    ;;
+    ;; WASM for VisualBasic.NET
+    ;; 
+    ;; version: 1.3.0.22
+    ;; build: 4/13/2019 8:01:09 PM
+
+    ;; imports must occur before all non-import definitions
+
+    ;; Declare Function DOMById Lib "document" Alias "getElementById" (id As char*) As i32
+(func $DOMById (import "document" "getElementById") (param $id i32) (result i32))
+    ;; Declare Function WriteText Lib "document" Alias "writeElementText" (dom As i32, text As char*) As i32
+(func $WriteText (import "document" "writeElementText") (param $dom i32) (param $text i32) (result i32))
+    
+    ;; Only allows one memory block in each module
+    (memory $Xf1010000 1)  
+
+    ;; Memory data for string constant
+    
+;; String from 1 with 12 bytes in memory
+(data (i32.const 1) "Hello world!")
+    
+    
+
+    ;; Export memory block to Javascript 
+    (export "memory" (memory $Xf1010000)) 
+
+    (export "sayHello" (func $sayHello)) 
+
+    (func $sayHello  (result i32)
+        ;; Public Function sayHello() As char*
+        (local $text char*)
+    (local $node f64)
+    (set_local $text (i32.const 1))
+    (set_local $node (f64.convert_s/i32 (call $DOMById (get_local $text))))
+    (call $WriteText (i32.trunc_s/f64 (get_local $node)) (get_local $text))
+    (return (get_local $text))
+    )
+
+    
+(export "MemorySizeOf" (func $MemorySizeOf))
+
+(func $MemorySizeOf (param $intPtr i32) (result i32)
+    
+    
+(if (i32.eq (get_local $intPtr) (i32.const 1)) 
+    (then
+                (return (i32.const 12))
+    ) 
+)
+
+    ;; pointer not found
+    (return (i32.const -1))
+)
+
+
+)
--- TsBuild/Wasm/Extensions.vb
+++ TsBuild/Wasm/Extensions.vb
@@ -1,44 +1,44 @@
 ﻿#Region "Microsoft.VisualBasic::b86a6d50ad4955c394efc5794be0eb25, Extensions.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (I@xieguigang.me)
-    ' 
-    ' Copyright (c) 2019 GCModeller Cloud Platform
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (I@xieguigang.me)
+' 
+' Copyright (c) 2019 GCModeller Cloud Platform
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Extensions
-    ' 
-    '     Function: CreateModule, (+2 Overloads) CreateModuleFromProject, isExportObject, param, SolveStream
-    ' 
-    ' /********************************************************************************/
+' Module Extensions
+' 
+'     Function: CreateModule, (+2 Overloads) CreateModuleFromProject, isExportObject, param, SolveStream
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -106,6 +106,6 @@ Public Module Extensions
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     <Extension>
     Friend Function param(a As NamedValue(Of String)) As String
-        Return $"(param ${a.Name} {a.Value})"
+        Return $"(param ${a.Name} {a.Value Or "i32".When(a.Value = Types.stringType)})"
     End Function
 End Module
