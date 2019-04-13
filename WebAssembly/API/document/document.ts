405f2330e1d33f9dbe9b3b5d9f1fa45031904683
xieguigang <xie.guigang@gcmodeller.org>
2019-04-13 19:28:21 +0800

    add dom api

---
+++ WebAssembly/API/document/document.ts
@@ -0,0 +1,15 @@
+﻿namespace WebAssembly {
+
+    export class document {
+
+        private streamReader: TypeScript.stringReader;
+
+        public constructor(public wasm: TypeScript.IWasm) {
+            this.streamReader = new TypeScript.stringReader(wasm);
+        }
+
+        public getElementById(id: number): HTMLElement {
+            return window.document.getElementById(this.streamReader.readText(id));
+        }
+    }
+}
