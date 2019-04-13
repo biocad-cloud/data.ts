d5c996eae40b5b758aa8d874f82f7c4320448b1c
xieguigang <xie.guigang@gcmodeller.org>
2019-04-14 02:04:19 +0800

    add basic console api

---
+++ WebAssembly/API/console.ts
@@ -0,0 +1,21 @@
+﻿namespace WebAssembly {
+
+    export module Console {
+
+        export function log(message: number) {
+            console.log(ObjectManager.readText(message));
+        }
+
+        export function warn(message: number) {
+            console.warn(ObjectManager.readText(message));
+        }
+
+        export function info(message: number) {
+            console.info(ObjectManager.readText(message));
+        }
+
+        export function error(message: number) {
+            console.error(ObjectManager.readText(message));
+        }
+    }
+}
