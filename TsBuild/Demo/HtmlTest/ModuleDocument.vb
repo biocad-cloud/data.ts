43bf8fd1035c02275e80d197ff92ee7511e52511
xieguigang <xie.guigang@gcmodeller.org>
2019-04-13 19:42:48 +0800

    add html demo api test

---
+++ TsBuild/Demo/HtmlTest/ModuleDocument.vb
@@ -0,0 +1,15 @@
+﻿Public Module ModuleDocument
+
+    Public Declare Function DOMById Lib "document" Alias "getElementById" (id As String) As Integer
+    Public Declare Function WriteText Lib "document" Alias "writeElementText" (dom As Integer, text As String) As Integer
+
+    Public Function sayHello() As String
+        Dim text As String = "Hello world!"
+        Dim node = DOMById(text)
+
+        Call WriteText(node, text)
+
+        Return text
+    End Function
+
+End Module
--- WebAssembly/API/document/document.ts
+++ WebAssembly/API/document/document.ts
@@ -3,13 +3,41 @@
     export class document {
 
         private streamReader: TypeScript.stringReader;
+        private hashCode: number;
+        private hashTable: object = {};
 
-        public constructor(public wasm: TypeScript.IWasm) {
-            this.streamReader = new TypeScript.stringReader(wasm);
+        public constructor(public wasm: TypeScript.IWasm = null) {
+            if (wasm && typeof wasm != "undefined") {
+                this.streamReader = new TypeScript.stringReader(wasm);
+            }
         }
 
-        public getElementById(id: number): HTMLElement {
-            return window.document.getElementById(this.streamReader.readText(id));
+        public hook(assembly: TypeScript.IWasm): document {
+            this.streamReader = new TypeScript.stringReader(assembly);
+            return this;
+        }
+
+        public getElementById(id: number): number {
+            let idText: string = this.streamReader.readText(id);
+            let node = window.document.getElementById(idText);
+
+            return this.addObject(node);
+        }
+
+        public writeElementText(key: number, text: number) {
+            let node: HTMLElement = this.hashTable[key];
+            let textVal: string = this.streamReader.readText(text);
+
+            node.innerText = textVal;
+        }
+
+        private addObject(o: any): number {
+            var key: number = this.hashCode;
+
+            this.hashTable[this.hashCode] = o;
+            this.hashCode++;
+
+            return key;
         }
     }
 }
