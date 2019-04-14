e5d7785a76104a0b536290694d4ed7119527095e
xieguigang <xie.guigang@gcmodeller.org>
2019-04-14 18:12:41 +0800

    regexp for VB.NET

--- WebAssembly/API/ObjectManager.ts
+++ WebAssembly/API/ObjectManager.ts
@@ -1,23 +1,46 @@
 ﻿namespace WebAssembly {
 
+    /**
+     * Object manager for VB.NET webassembly application.
+    */
     export module ObjectManager {
 
         let streamReader: TypeScript.stringReader;
         let hashCode: number = 0;
         let hashTable: object = {};
 
+        /**
+         * Load WebAssembly memory buffer into Javascript runtime.
+        */
         export function load(bytes: TypeScript.WasmMemory): void {
             streamReader = new TypeScript.stringReader(bytes);
         }
 
+        /**
+         * Read text data from WebAssembly runtime its memory block
+         * 
+         * @param intptr The memory pointer
+        */
         export function readText(intptr: number): string {
             return streamReader.readText(intptr);
         }
 
+        /**
+         * Get a object using its hash code
+         * 
+         * @returns If object not found, null will be returns
+        */
         export function getObject(key: number): any {
-            return hashTable[key];
+            if (key in hashTable) {
+                return hashTable[key];
+            } else {
+                return null;
+            }
         }
 
+        /**
+         * Add any object to a internal hashTable and then returns its hash code.
+        */
         export function addObject(o: any): number {
             var key: number = hashCode;
 
---
+++ WebAssembly/API/regexp.ts
@@ -0,0 +1,21 @@
+﻿namespace WebAssembly {
+
+    export module RegularExpression {
+
+        export function regexp(pattern: number, flags: number): number {
+            let patternText: string = ObjectManager.readText(pattern)
+            let r = new RegExp(patternText, ObjectManager.readText(flags))
+
+            return ObjectManager.addObject(r);
+        }
+
+        export function replace(text: number, pattern: number, replacement: number): number {
+            let input: string = ObjectManager.readText(text)
+            let patternObj: RegExp = ObjectManager.getObject(pattern)
+            let replaceAs: string = ObjectManager.readText(replacement);
+            let result: string = input.replace(patternObj, replaceAs);
+
+            return ObjectManager.addObject(result);
+        }
+    }
+}
