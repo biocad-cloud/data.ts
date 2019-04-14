b5fefe8dc7266445952c3dfc6d84adb06bbc26ad
xieguigang <xie.guigang@gcmodeller.org>
2019-04-14 15:22:45 +0800

    add test

---
+++ TsBuild/Wasm/test/EnumTest.vb
@@ -0,0 +1,29 @@
+﻿Module EnumTest
+
+    Public Enum asInteger As Integer
+        A = 1
+        B
+        C
+        D
+        E
+        F
+        G
+    End Enum
+
+    Public Enum asLong As Long
+        A = 1
+        B
+        C
+        E
+        D
+        F
+        G
+    End Enum
+
+    Public Function Add1(i As asInteger) As asLong
+        Dim x = i + 1
+        Dim a = CType(x, asLong)
+
+        Return a
+    End Function
+End Module
