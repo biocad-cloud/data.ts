dbeef32fa981f722cc1812161580d2f719472b5d
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-04-15 23:03:08 +0800

    add enum object model

---
+++ TsBuild/Wasm/Symbols/EnumSymbol.vb
@@ -0,0 +1,33 @@
+﻿Namespace Symbols
+
+    ''' <summary>
+    ''' The enum type object model
+    ''' </summary>
+    Public Class EnumSymbol
+
+        ''' <summary>
+        ''' The enum type name
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Name As String
+        ''' <summary>
+        ''' WebAssembly Type: i32 or i64
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property type As String
+
+        ''' <summary>
+        ''' [member name => value]
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Members As Dictionary(Of String, String)
+
+        Sub New()
+
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return $"Dim {Name} As {type}"
+        End Function
+    End Class
+End Namespace
