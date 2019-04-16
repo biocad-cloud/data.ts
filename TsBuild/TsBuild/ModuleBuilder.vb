16b95b504857639940a5ade34597d3171d8b3bb0
xieguigang <xie.guigang@gcmodeller.org>
2019-04-16 09:44:56 +0800

    yield token by type

--- TsBuild/TsBuild/ModuleBuilder.vb
+++ TsBuild/TsBuild/ModuleBuilder.vb
@@ -13,9 +13,18 @@ Public Class ModuleBuilder
     Public Iterator Function ParseIndex(text As String) As IEnumerable(Of Token)
         Dim code As New Pointer(Of Char)(text.SolveStream)
         Dim c As Value(Of Char) = ""
+        Dim type As Value(Of String) = ""
 
         Do While (c = ++code) <> ASCII.NUL
-            Call walkChar(c)
+            If Not String.IsNullOrEmpty(type = walkChar(c)) AndAlso buffer > 0 Then
+                Yield New Token With {
+                    .Text = buffer.CharString,
+                    .Type = type
+                }
+
+                ' clear buffer
+                buffer *= 0
+            End If
         Loop
     End Function
 
@@ -23,9 +32,51 @@ Public Class ModuleBuilder
         Return buffer.SequenceEqual(test)
     End Function
 
-    Private Sub walkChar(c As Char)
+    Private Function bufferStartWith(test As String) As Boolean
+        Return buffer.Take(test.Length).SequenceEqual(test)
+    End Function
 
-    End Sub
+    Private Function bufferEndWith(test As String) As Boolean
+        Return buffer.Skip(buffer.Count - test.Length).SequenceEqual(test)
+    End Function
+
+    Private Function walkChar(c As Char) As String
+        If escape.SingleLineComment Then
+            If c = ASCII.CR OrElse c = ASCII.LF Then
+                ' 单行注释在遇到换行符之后结束
+                escape.SingleLineComment = False
+                Return "comment"
+            Else
+                buffer += c
+            End If
+        ElseIf escape.BlockTextComment Then
+            buffer += c
+
+            If bufferEndWith("*/") Then
+                escape.BlockTextComment = False
+                Return "comment"
+            End If
+        End If
+
+        If c = "/"c Then
+            buffer += "/"c
+
+            If bufferEquals("//") Then
+                escape.SingleLineComment = True
+            Else
+                Throw New SyntaxErrorException
+            End If
+        ElseIf c = " "c Then
+            ' a string delimiter
+            If bufferEndWith(":") Then
+                Return "identifier"
+            ElseIf bufferEndWith("(") Then
+                Return "funcName"
+            End If
+        End If
+
+        Return Nothing
+    End Function
 End Class
 
 Public Class Token
