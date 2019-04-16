a89dd51b530fef0131bf144b6ea85f489323cbc8
xieguigang <xie.guigang@gcmodeller.org>
2019-04-16 11:46:17 +0800

    add debug view

--- TsBuild/TsBuild/ModuleBuilder.vb
+++ TsBuild/TsBuild/ModuleBuilder.vb
@@ -107,6 +107,10 @@ Public Class Token
     Public Property Text As String
     Public Property Type As TypeScriptTokens
 
+    Public Overrides Function ToString() As String
+        Return $"[{Type}] {Text}"
+    End Function
+
 End Class
 
 Public Class Escapes
