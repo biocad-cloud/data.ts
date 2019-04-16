9e6c30474829fb5e1fffe88953dd4d524170adca
xieguigang <xie.guigang@gcmodeller.org>
2019-04-16 22:57:58 +0800

    split source file for project compiler test

---
+++ TsBuild/Demo/vbscript/HelloWorld/console.vb
@@ -0,0 +1,15 @@
+﻿
+''' <summary>
+''' The javascript debugconsole api
+''' </summary>
+Module console
+
+#Region "Javascript console api"
+
+    Declare Function log Lib "console" Alias "log" (message As String) As Integer
+    Declare Function warn Lib "console" Alias "warn" (message As String) As Integer
+    Declare Function info Lib "console" Alias "info" (message As String) As Integer
+    Declare Function [error] Lib "console" Alias "error" (message As String) As Integer
+
+#End Region
+End Module
