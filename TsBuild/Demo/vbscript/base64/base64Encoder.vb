0e381921704e285292374d302dea05f122040e04
xieguigang <xie.guigang@gcmodeller.org>
2019-04-20 12:15:02 +0800

    write base64encoder demo  test

--- TsBuild/Demo/vbscript/base64/base64Encoder.vb
+++ TsBuild/Demo/vbscript/base64/base64Encoder.vb
@@ -10,12 +10,117 @@ Public Module base64Encoder
     ''' </summary>
     ''' <param name="pattern"></param>
     ''' <returns></returns>
-    Declare Function regexp Lib "regexp" Alias "regexp" (pattern As String) As Integer
+    Declare Function regexp Lib "regexp" Alias "regexp" (pattern As String, Optional flag$ = "g") As Integer
     Declare Function push Lib "array" Alias "push" (array As Array, element As Object) As Integer
     Declare Function fromCharCode Lib "String" Alias "fromCharCode" (c As Integer) As Char
-    Declare Function Join Lib "String" Alias "Join" (array As Array, delimiter As String) As String
 
-    Dim symbolsNotallowed As Integer = regexp("[^A-Za-z0-9+/=]")
+    <Extension>
+    Declare Function charCodeAt Lib "String" Alias "CharCodeAt" (text As String, index As Integer) As Integer
+    <Extension>
+    Declare Function charAt Lib "String" Alias "CharAt" (text As String, index As Integer) As String
+
+    Declare Function isNaN Lib "number" Alias "isNaN" (x As Integer) As Boolean
+
+    <Extension>
+    Declare Function Join Lib "String" Alias "Join" (array As IList, delimiter As String) As String
+
+    ''' <summary>
+    ''' 将任意文本编码为base64字符串
+    ''' </summary>
+    ''' <param name="text"></param>
+    ''' <returns></returns>
+    Public Function encode(text As String) As String
+        Dim base64 As List(Of String) = New List(Of String)
+        Dim n, r, i, s, o, u, a As Integer
+        Dim f = 0
+
+        text = base64Encoder.utf8_encode(text)
+
+        Do While (f < text.Length)
+            n = text.charCodeAt(++f)
+            r = text.charCodeAt(++f)
+            i = text.charCodeAt(++f)
+            s = n >> 2
+            o = (n And 3) << 4 Or r >> 4
+            u = (r And 15) << 2 Or i >> 6
+            a = i And 63
+
+            If (isNaN(r)) Then
+                a = 64
+                u = a
+            ElseIf (isNaN(i)) Then
+                a = 64
+            End If
+
+            base64.Add(keyStr.charAt(s))
+            base64.Add(keyStr.charAt(o))
+            base64.Add(keyStr.charAt(u))
+            base64.Add(keyStr.charAt(a))
+        Loop
+
+        Return base64.Join("")
+    End Function
+
+    Public Function decode(base64 As String) As String
+        Dim text = ""
+        Dim n, r, i As Integer
+        Dim s, o, u, a As Integer
+        Dim f = 0
+        Dim symbolsNotallowed As Integer = regexp("[^A-Za-z0-9+/=]", "g")
+
+        base64 = base64.Replace(symbolsNotallowed, "")
+
+        Do While (f < base64.Length)
+            s = keyStr.IndexOf(base64.charAt(++f))
+            o = keyStr.IndexOf(base64.charAt(++f))
+            u = keyStr.IndexOf(base64.charAt(++f))
+            a = keyStr.IndexOf(base64.charAt(++f))
+            n = s << 2 Or o >> 4
+            r = (o And 15) << 4 Or u >> 2
+            i = (u And 3) << 6 Or a
+            text = text + fromCharCode(n)
+
+            If (u <> 64) Then
+                text = text + fromCharCode(r)
+            End If
+            If (a <> 64) Then
+                text = text + fromCharCode(i)
+            End If
+        Loop
+
+        text = base64Encoder.utf8_decode(text)
+
+        Return text
+    End Function
+
+
+    ''' <summary>
+    ''' 将文本转换为utf8编码的文本字符串
+    ''' </summary>
+    ''' <param name="text"></param>
+    ''' <returns></returns>
+    Public Function utf8_encode(text As String) As String
+        Dim chars As List(Of String) = New List(Of String)
+
+        text = Replace(text, regexp("rn", "g"), "n")
+
+        For n As Integer = 0 To text.Length - 1
+            Dim r = charCodeAt(text, n)
+
+            If (r < 128) Then
+                chars.Add(fromCharCode(r))
+            ElseIf (r > 127 AndAlso r < 2048) Then
+                chars.Add(fromCharCode(r >> 6 Or 192))
+                chars.Add(fromCharCode(r And 63 Or 128))
+            Else
+                chars.Add(fromCharCode(r >> 12 Or 224))
+                chars.Add(fromCharCode(r >> 6 And 63 Or 128))
+                chars.Add(fromCharCode(r And 63 Or 128))
+            End If
+        Next
+
+        Return chars.Join("")
+    End Function
 
     ''' <summary>
     ''' 将utf8编码的文本转换为原来的文本
@@ -23,7 +128,7 @@ Public Module base64Encoder
     ''' <param name="text"></param>
     ''' <returns></returns>
     Public Function utf8_decode(text As String) As String
-        Dim t As String() = {}
+        Dim t As List(Of String) = New List(Of String)
         Dim n = 0
         Dim r = 0
         Dim c2 = 0
@@ -33,25 +138,20 @@ Public Module base64Encoder
             r = text.charCodeAt(n)
 
             If (r < 128) Then
-                push(t, fromCharCode(r))
+                t.Add(fromCharCode(r))
                 n += 1
             ElseIf (r > 191 AndAlso r < 224) Then
                 c2 = text.charCodeAt(n + 1)
-                push(t, fromCharCode((r And 31) << 6 Or c2 And 63))
+                t.Add(fromCharCode((r And 31) << 6 Or c2 And 63))
                 n += 2
             Else
                 c2 = text.charCodeAt(n + 1)
                 c3 = text.charCodeAt(n + 2)
-                push(t, fromCharCode((r And 15) << 12 Or (c2 And 63) << 6 Or c3 And 63))
+                t.Add(fromCharCode((r And 15) << 12 Or (c2 And 63) << 6 Or c3 And 63))
                 n += 3
             End If
         Loop
 
-        Return Join(t, "")
-    End Function
-
-    <Extension>
-    Public Function charCodeAt(text As String, n As Integer) As Integer
-        Return Asc(text(n))
+        Return t.Join("")
     End Function
 End Module
