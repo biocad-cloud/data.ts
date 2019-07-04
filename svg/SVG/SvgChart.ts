2b12a6473b6af9cffddfabc213f59e665a522ad9
xieguigang <xie.guigang@gcmodeller.org>
2019-07-04 14:26:00 +0800

    abstract chart model

---
+++ svg/SVG/SvgChart.ts
@@ -0,0 +1,18 @@
+﻿abstract class SvgChart {
+
+    public size: [number, number];
+    public margin: Canvas.Margin;
+
+    public constructor(
+        size: Canvas.Size | number[] = [960, 600],
+        margin: Canvas.Margin = <Canvas.Margin>{
+            top: 20, right: 20, bottom: 30, left: 40
+        }) {
+
+        if (!Array.isArray(size)) {
+            this.size = [size.width, size.height];
+        }
+
+        this.margin = margin;
+    }
+}
