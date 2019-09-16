8b5701799cbaf0476cbf30c7759563ddcab1b011
xieguigang <xie.guigang@gcmodeller.org>
2019-09-16 14:28:55 +0800

    custom event

---
+++ LINQ.ts/DOM/Events/CustomEvents.ts
@@ -0,0 +1,32 @@
+﻿namespace DOM.Events {
+
+    var started: boolean;
+    var customEvents: {
+        hasUpdate: Delegate.Func<boolean>,
+        invoke: Delegate.Sub,
+        name?: string
+    }[] = [];
+
+    /**
+     * Add custom user event.
+     * 
+     * @param trigger This lambda function detects that custom event is triggered or not.
+     * @param handler This lambda function contains the processor code of your custom event.
+    */
+    export function Add(trigger: Delegate.Func<boolean>, handler: Delegate.Sub, tag: string = null) {
+        customEvents.push({ hasUpdate: trigger, invoke: handler, name: tag });
+
+        if (!started) {
+            setInterval(backgroundInternal, 10);
+            started = true;
+        }
+    }
+
+    function backgroundInternal() {
+        for (let hook of customEvents) {
+            if (hook.hasUpdate()) {
+                hook.invoke();
+            }
+        }
+    }
+}
