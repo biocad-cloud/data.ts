6b145f423661c7b36c79ab77de4e3d7250efe209
xieguigang <xie.guigang@gcmodeller.org>
2019-09-27 17:03:18 +0800

    background worker helper

---
+++ LINQ.ts/Framework/BackgroundWorker.ts
@@ -0,0 +1,26 @@
+﻿namespace Internal {
+
+    export class BackgroundWorker {
+
+        public static $workers = {};
+
+        public get hasWorkerFeature(): boolean {
+            if (typeof (Worker) !== "undefined") {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public static RunWorker(script: string, onMessage: Delegate.Sub) {
+            BackgroundWorker.$workers[script] = new Worker(script);
+            BackgroundWorker.$workers[script].onmessage = onMessage;
+        }
+
+        public static Stop(script: string) {
+            BackgroundWorker.$workers[script].terminate();
+            // removes worker object
+            delete BackgroundWorker.$workers[script];
+        }
+    }
+}
