3bf0b06b0840e18f49eac077af796602288aeb85
xieguigang <xie.guigang@gcmodeller.org>
2019-10-14 10:29:56 +0800

    parse inline css

--- LINQ.ts/DOM/CSS/CSSModel.ts
+++ LINQ.ts/DOM/CSS/CSSModel.ts
@@ -5,5 +5,37 @@
         styles: NamedValue<string>[];
     }
 
-   
+    /**
+     * 解析选择器所对应的样式配置信息
+    */
+    export function parseStylesheet(content: string): NamedValue<string>[] {
+        let i = new Pointer<string>(CharEnumerator(content));
+        let stylesheet: NamedValue<string>[] = [];
+        let buf: string[] = [];
+        let name: string;
+        let value: string;
+        let c: string;
+
+        while (!i.EndRead) {
+            c = i.MoveNext().Current;
+
+            if (c == ":") {
+                name = buf.join("");
+                buf = [];
+            } else if (c == ";") {
+                value = buf.join("");
+                buf = [];
+
+                stylesheet.push(new NamedValue(name, value));
+            } else {
+                buf.push(c);
+            }
+        }
+
+        if (buf.length > 0) {
+            stylesheet.push(new NamedValue(name, buf.join("")));
+        }
+
+        return stylesheet;
+    }
 }
---
+++ LINQ.ts/DOM/CSS/CSSSetter.ts
@@ -0,0 +1,7 @@
+﻿namespace DOM.CSS.Setter {
+
+    export function setStyle(node: HTMLElement, style: NamedValue<string>[]) {
+
+    }
+
+}
