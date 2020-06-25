a94e88e126030d0b11383e1a6116e82ff9709775
xieguigang <xie.guigang@gcmodeller.org>
2020-06-25 09:44:37 +0800

    improvements for handle click event

--- LINQ.ts/Framework/EventHandles.ts
+++ LINQ.ts/Framework/EventHandles.ts
@@ -22,12 +22,26 @@ namespace Internal {
 
             for (let methodName of type.methods) {
                 if (elements.indexOf(methodName) > -1) {
-                    let method: Delegate.Action = app[methodName];
-
-                    $ts(`#${methodName}`).onclick = method;
+                    $ts(`#${methodName}`).onclick = <any>function (handler, evt): any {
+                        return app[methodName](handler, evt);
+                    }
 
                     console.info(`%c[${type.class}] hook onclick for #${methodName}...`, "color:green;");
                 }
+            }
+        }
+
+        const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+        const ARGUMENT_NAMES = /([^\s,]+)/g;
+
+        export function parseFunctionArgumentNames(func: any): string[] {
+            let fnStr = func.toString().replace(STRIP_COMMENTS, '');
+            let result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+
+            if (result === null) {
+                return [];
+            } else {
+                return result;
             }
         }
     }
