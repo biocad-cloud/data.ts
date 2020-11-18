9f3b5a3cfa94dc6c92e354e28525ea6545fc7972
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-11-18 15:06:35 +0800

    download excel file

--- LINQ.ts/DOM/Excel.ts
+++ LINQ.ts/DOM/Excel.ts
@@ -1,6 +1,50 @@
 namespace DOM.Excel {
 
-    export function excel(table: HTMLTableElement, fileName: string, sheetName: string) {
+    export const contentType: string = "application/vnd.ms-excel";
 
+    export function excel(table: HTMLTableElement, fileName: string, sheetName: string) {
+        DOM.download(fileName, <DataURI>{
+            mime_type: Excel.contentType,
+            data: Base64.encode(ToExcel(table, sheetName))
+        });
+    }
+
+    export function ToExcel(table: HTMLTableElement, sheetName: string): string {
+        let html = new StringBuilder("", "\n");
+
+        html.AppendLine(`<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40">`);
+        html.AppendLine(`<head>`);
+        html.AppendLine(`<meta name="ProgId" content="Excel.Sheet">`);
+        html.AppendLine(`<meta name="Generator" content="Microsoft Excel 11">`);
+        html.AppendLine(`<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">`);
+        html.AppendLine(`    
+<!--[if gte mso 9]>
+	<xml>
+		<x:ExcelWorkbook>
+			<x:ExcelWorksheets>
+				<x:ExcelWorksheet>
+					<x:Name>${sheetName}</x:Name>
+					<x:WorksheetOptions>
+						<x:DisplayGridlines/>
+					</x:WorksheetOptions>
+				</x:ExcelWorksheet>
+			</x:ExcelWorksheets>
+		</x:ExcelWorkbook>
+	</xml>
+<![endif]-->
+`);
+        html.AppendLine(`</head>`);
+        html.AppendLine(`<body>`);
+        html.AppendLine(ToHtml(table));
+        html.AppendLine(`</body>`);
+        html.AppendLine(`</html>`);
+
+        return html.toString();
+    }
+
+    export function ToHtml(table: HTMLTableElement): string {
+        let html = new StringBuilder("", "\n");
+
+        return html.toString();
     }
 }
